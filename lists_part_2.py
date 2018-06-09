1b2d3654ed75f9e3412511efd18814ff9693cb45
Radu1990 <39800677+Radu1990@users.noreply.github.com>
2018-06-09 12:05:10 +0200

    Update lists_part_2.py

--- lists_part_2.py
+++ lists_part_2.py
@@ -7,3 +7,15 @@ while True:
 
 average = sum(numlist) / len(numlist)
 print('Average number is:', average)
+
+###same program written manually
+total = 0
+count = 0
+while True:
+    inp = input('Enter a number YO! : ')
+    if inp == 'done' : break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+average = total / count
+print("Average: ", average)
