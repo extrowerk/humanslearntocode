f685707d5f05be9c52c42327ec7777ab47c5a032
Radu1990 <39800677+Radu1990@users.noreply.github.com>
2018-06-09 12:27:05 +0200

    Create split_lists.py

---
+++ split_lists.py
@@ -0,0 +1,10 @@
+abc = ('QWERTY IS KING')
+stuff = abc.split()
+print(stuff)
+print(len(stuff))
+print(stuff[0])
+print(stuff[1])
+print(stuff[2])
+print('End of story bro!')
+for w in stuff:
+    print('Ma men', w)
