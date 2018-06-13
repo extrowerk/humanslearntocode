2051e2395f0d32b3ed12838036daf70820c83097
Radu1990 <39800677+Radu1990@users.noreply.github.com>
2018-06-13 15:41:33 +0200

    Create assignment_10.2.py

---
+++ assignment_10.2.py
@@ -0,0 +1,36 @@
+
+######################################################
+#                                                    #
+# Thank you Charles for the wonderful course so far! #
+#                                                    #
+######################################################
+
+name = input("Enter file:")
+if len(name) < 1: name = "mbox-short.txt"
+handle = open(name)
+counts = dict()
+
+for line in handle:
+    line = line.strip()
+    if not line.startswith('From') : continue
+    if not line.endswith('2008') : continue
+
+    list_0 = line.split() #here we break the line in words
+    time = list_0[5] #here we select the time from the sequence where time is the 5th element
+    time = list_0[5].split(":") #here we break the time string using the : symbol into more pieces
+    hour = time[0] #here we assing to hour variable the first piece from the secquence above which is the hour
+
+    if hour not in counts:
+        counts[hour] = 1
+    else:
+        counts[hour] += 1 #here we create a dictionary where we create a list of hours in the dictionary using keys as hours and incrementing values
+
+    list_1 = list(counts.items()) #here we create a new list where we bring all the elements (key-value) from the dictionary
+    list_1.sort() #here we sort the list after the keys
+
+for key, value in list_1:
+    print(key, value) #in the end we choose to print the key, values from the sorted list
+
+
+
+
